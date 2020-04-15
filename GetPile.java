ad616be85c81aa7022d2afdf89d6a77447917138
Peter Simari <petersimari@gmail.com>
2020-04-15 13:35:13 -0400

    Update GetPile.java

--- GetPile.java
+++ GetPile.java
@@ -1,3 +1,4 @@
+import javafx.scene.canvas.Canvas;
 
 public class GetPile extends Pile {
 
@@ -39,5 +40,28 @@ public class GetPile extends Pile {
       }
     }
     return -1;
+  }
+
+  public Canvas toNode(int offset) {
+    Canvas canvas = new Canvas(100, 150);
+
+    if (cards.size() == 0) {
+      base.drawEmptyOnCanvas(canvas, 0, 0);
+      return canvas;
+    }
+
+    for (int i = 0; i < cards.size(); i++) {
+      // canvas.drawImage(c.toImage(), 0, 0);
+      if (this.selected) {
+
+        cards.get(i).drawEmptyOnCanvas(canvas, 0, i * offset);
+        // System.out.println("Selected");
+      } else {
+        cards.get(i).drawOnCanvas(canvas, 0, i * offset);
+      }
+
+    }
+
+    return canvas;
   }
 }
