fbce2a507730b6b5e2003ebb36cae81b8c00e1d3
Peter Simari <petersimari@gmail.com>
2020-04-20 14:38:07 -0400

    BUGS
    so many are finished :)

--- NormalPile.java
+++ NormalPile.java
@@ -19,33 +19,51 @@ public class NormalPile extends Pile {
 
     System.out.println("We're in willMove Normal");
     for (int i = this.cards.size() - 1; i >= 0; i--) {
-      System.out.println(i);
+      System.out.println("i = " + i);
       topCard = this.cards.get(i);
+      System.out.println("Top: " + topCard.toString());
+      System.out.println("topCard rank? " + topCard.rank);
+      if (p.cards.isEmpty() && topCard.rank == 13 && p.type == PileType.Normal) {
 
-      // System.out.println("topCard rank? " + topCard.rank);
-      if (p.cards.isEmpty() && topCard.rank == 13) {
         return i;
         // newCard = p.cards.get(p.cards.size() - 1);
-      } else if (p.cards.isEmpty() && topCard.rank != 13) {
+      } else if (p.cards.isEmpty() && topCard.rank == 1 && p.type == PileType.Final) {
         System.out.println("Is p.cards empty?: " + p.cards.isEmpty());
         System.out.println("This is a lot of damage");
-        // return -1;
+        return i;
+        // continue;
+      } else if (p.cards.isEmpty() && topCard.rank != 13 && p.type != PileType.Final) {
         continue;
       }
       newCard = p.peekTop();
-      // System.out.println("Top: " + topCard.toString());
-      // System.out.println("New:" + newCard.toString());
       if (!topCard.face) {
         return -1;
       }
+      if (p.type == PileType.Final || this.type == PileType.Final) {
+        if (topCard.rank - 1 == newCard.rank && topCard.suit == newCard.suit) {
+          System.out.println("P.type = " + p.type);
+          System.out.println("this.type = " + this.type);
+          System.out.println("If the toprank == newCard rank");
+          return 1;
+        }
+      }
+      System.out.println("Top: " + topCard.toString());
+      System.out.println("New:" + newCard.toString());
+
+      // if (!topCard.face) {
+      //   System.out.println("Face is wrong");
+      //   return -1;
+      // }
 
       if (topCard.black ^ newCard.black) {
         if (topCard.rank + 1 == newCard.rank) {
-          // System.out.println("CAN MOVE TRUE");
+          System.out.println("CAN MOVE TRUE");
           return i;
         }
       }
     }
+    // System.out.println("CAN MOVE TRUE");
+    System.out.println("Last resort with Stan");
     return -1;
   }
 
