2da829d04251e92a0fdae071cc3617a398169614
Peter Simari <petersimari@gmail.com>
2020-04-27 14:14:48 -0400

    Javadoc

--- ThemeView.java
+++ ThemeView.java
@@ -1,5 +1,11 @@
+/**
+ * State pattern to determine what theme is currently active.
+ */
 public interface ThemeView {
 
+  /**
+   * Updates what theme is being used. Different for each theme java class.
+   */
   public void handleTheme();
 
 }
