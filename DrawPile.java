7a1dd9c514b37c772da1d00a7186e4dec137547f
Peter Simari <petersimari@gmail.com>
2020-04-27 14:14:26 -0400

    Javadoc

--- DrawPile.java
+++ DrawPile.java
@@ -1,12 +1,21 @@
 import javafx.scene.canvas.Canvas;
 
+/**
+ * Draw pile denotes the pile that is in the top right of the screen.
+ * The cards you pull from to add to the GetPile.
+ */
 public class DrawPile extends Pile {
 
+  /**
+   * Creates a regular pile, but setting the pile type to Draw.
+   */
   public DrawPile() {
     setPileType(PileType.Draw);
   }
 
-
+  /**
+   * Draws the drawPile without an offset.
+   */
   public Canvas toNode(int offset) {
     Canvas canvas = new Canvas(100, 150);
 
@@ -16,17 +25,12 @@ public class DrawPile extends Pile {
     }
 
     for (int i = 0; i < cards.size(); i++) {
-      // canvas.drawImage(c.toImage(), 0, 0);
       if (this.selected) {
-
         cards.get(i).drawSelectOnCanvas(canvas, 0, i * offset);
-        // System.out.println("Selected");
       } else {
         cards.get(i).drawOnCanvas(canvas, 0, i * offset);
       }
-
     }
-
     return canvas;
   }
 }
